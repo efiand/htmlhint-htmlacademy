ddeea555ccd71815bbf2c50f7be305d1b8ef74fa
Андрей Ефимов <efiand@ya.ru>
2020-09-12 16:22:46 +0300

    Версия 2.0.0 Универсальный модуль обновления данных

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 'use strict';
 
-let data = {};
-
 // обновление настроек из репозитория кодгайда HTML Academy
-if (~process.argv.indexOf(`-u`)) {
-  const fs = require(`fs`);
-  const fetch = require(`node-fetch`);
-  const { blueBright, green, red } = require(`chalk`);
-  const packageName = `[htmlhint-htmlacademy]`;
-  const packagePrefix = blueBright(`${packageName}: `);
-  const dirname = __dirname.replace(/\\/g, `/`);
-
-  const updateData = (body) => {
-    // разрешение svg-атрибутов (https://github.com/htmlacademy/codeguide/issues/32)
+if (process.argv.indexOf(`-u`) !== -1) {
+  require(`./update`)((body) => {
     if (typeof body[`attr-lowercase`] !== `object`) {
       body[`attr-lowercase`] = [];
     }
     let attrLower = body[`attr-lowercase`];
+
+    // разрешение svg-атрибутов (https://github.com/htmlacademy/codeguide/issues/32)
     if (!~attrLower.indexOf(`viewBox`)) {
       attrLower.push(`viewBox`);
     }
@@ -27,20 +19,8 @@ if (~process.argv.indexOf(`-u`)) {
     // БЭМ-совместимая конфигурация
     body[`id-class-value`] = false;
 
-    data = JSON.stringify(body);
-    fs.writeFile(`${dirname}/htmlhintrc.json`, data, () => {
-      console.info(packagePrefix + green(`properties list successfully updated!`));
-    });
-  };
-
-  fetch(`https://raw.githubusercontent.com/htmlacademy/codeguide/master/.htmlhintrc`)
-    .then((res) => res.json())
-    .then(updateData)
-    .catch(() => {
-      console.error(packagePrefix + red(`update error, run "${packageName} -u"`));
-    });
-} else {
-  data = require(`./htmlhintrc.json`);
+    return body;
+  });
 }
 
-module.exports = data;
+module.exports = require(`./data.json`);
