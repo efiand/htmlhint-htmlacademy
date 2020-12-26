e64625dcb1354d7642254ed6b63b1c04f34ec3ed
Андрей Ефимов <efiand@ya.ru>
2020-12-26 13:51:53 +0300

    2.0.1 fix url

--- index.js
+++ index.js
@@ -2,25 +2,7 @@
 
 // обновление настроек из репозитория кодгайда HTML Academy
 if (process.argv.indexOf(`-u`) !== -1) {
-  require(`./update`)((body) => {
-    if (typeof body[`attr-lowercase`] !== `object`) {
-      body[`attr-lowercase`] = [];
-    }
-    let attrLower = body[`attr-lowercase`];
-
-    // разрешение svg-атрибутов (https://github.com/htmlacademy/codeguide/issues/32)
-    if (!~attrLower.indexOf(`viewBox`)) {
-      attrLower.push(`viewBox`);
-    }
-    if (!~attrLower.indexOf(`preserveAspectRatio`)) {
-      attrLower.push(`preserveAspectRatio`);
-    }
-
-    // БЭМ-совместимая конфигурация
-    body[`id-class-value`] = false;
-
-    return body;
-  });
+  require(`./update`)((body) => body);
 }
 
 module.exports = require(`./data.json`);
